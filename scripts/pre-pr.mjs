6f74f76155c5bbf2aa5ba19f23c354f4248dfa67
Matt <mossipcams@gmail.com>
2026-04-28 11:12:44 -0500

    chore: add preflight hooks for push and pr

---
+++ scripts/pre-pr.mjs
@@ -0,0 +1,10 @@
+import { runCi, runCommand } from "./preflight.mjs";
+
+const ciStatus = runCi();
+
+if (ciStatus !== 0) {
+  process.exitCode = ciStatus;
+} else {
+  console.log("\n> gh pr create");
+  process.exitCode = runCommand("gh", ["pr", "create", ...process.argv.slice(2)]);
+}
